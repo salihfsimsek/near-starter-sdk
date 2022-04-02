3a184d9544c37f6d26a1a20be39f5452a3a082c1
Salih Simsek <salihsimseek@gmail.com>
2022-04-02 16:19:21 +0300

    printed hello world

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -2,8 +2,7 @@ import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
 export function helloWorld(): string {
-  const predecessor = Context.predecessor
-  return`hello ${predecessor}`
+  return`hello world`
 }
 
 // read the given key from account (contract) storage
